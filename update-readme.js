abdf17605726de6dcb6db9e4f1c3a0b3edb97e9c
Giselle Ferreira <84051263+giselle-ferreira@users.noreply.github.com>
2023-09-29 10:58:37 -0300

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,20 +1,40 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const fs = require("fs/promises");
+const fetch = require('node-fetch');
+const fs = require('fs');
 
 async function updateReadme() {
-  const response = await axios.get("https://dev.to/giselletech");
-  const $ = cheerio.load(response.data);
+  const devToUsername = 'giselletech'; // Substitua pelo seu nome de usuário no Dev.to
 
-  const articles = [];
-  $(".single-article").each((_, element) => {
-    const title = $(element).find(".title").text().trim();
-    const url = $(element).find("a").attr("href");
-    articles.push(`- [${title}](${url})`);
+  const response = await fetch(`https://dev.to/api/articles?username=${devToUsername}`, {
+    headers: {
+      'api-key': process.env.DEVTO_API_TOKEN // Usa o token do Dev.to armazenado em secrets
+    }
   });
 
-  const readmeContent = `# Meus Artigos no Dev.to\n\n${articles.join("\n")}`;
-  await fs.writeFile("README.md", readmeContent);
+  if (!response.ok) {
+    console.error('Erro ao buscar artigos do Dev.to:', response.statusText);
+    return;
+  }
+
+  const articles = await response.json();
+
+  // Lê o conteúdo atual do README.md
+  const readmeContent = fs.readFileSync('README.md', 'utf8');
+
+  // Encontra a posição dos marcadores
+  const startMarker = '<!-- START_DEVTO_ARTICLES -->';
+  const endMarker = '<!-- END_DEVTO_ARTICLES -->';
+  const startIndex = readmeContent.indexOf(startMarker);
+  const endIndex = readmeContent.indexOf(endMarker);
+
+  if (startIndex !== -1 && endIndex !== -1) {
+    // Atualiza a parte entre os marcadores com a lista de artigos
+    const updatedContent = readmeContent.substring(0, startIndex + startMarker.length) +
+      articles.map(article => `- [${article.title}](${article.url})`).join('\n') +
+      readmeContent.substring(endIndex);
+
+    // Escreve o conteúdo atualizado de volta no README.md
+    fs.writeFileSync('README.md', updatedContent);
+  }
 }
 
 updateReadme();
